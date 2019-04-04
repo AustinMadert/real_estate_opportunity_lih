f91be3fc42e80da135c80a41da5d8029e401d068
Austin Madert <austinmadert@Austins-MacBook-Pro.local>
2019-04-04 10:41:18 -0500

    Updated settings.py to include Autothrottle lambda value

--- src/scraping/trulia/settings.py
+++ src/scraping/trulia/settings.py
@@ -68,7 +68,7 @@ DOWNLOADER_MIDDLEWARES = {
 # Enable or disable extensions
 # See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
 EXTENSIONS = {
-   'scrapy.extensions.': None,
+   'scrapy.extensions.Au': None,
 }
 
 # Configure item pipelines
@@ -89,6 +89,8 @@ AUTOTHROTTLE_MAX_DELAY = 60
 AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
 AUTOTHROTTLE_DEBUG = False
+# Added value for the exponential distribution 
+AUTOTHROTTLE_LAMBDA = 0.2
 
 # Enable and configure HTTP caching (disabled by default)
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
