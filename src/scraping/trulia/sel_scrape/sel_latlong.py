997a3334cc7fd8b5b9ff2ce3b881a7266a43ce36
Austin Madert <austinmadert@Austins-MacBook-Pro.local>
2019-04-08 10:04:55 -0500

    Wire-framed the sel_scraper

--- src/scraping/trulia/sel_scrape/sel_latlong.py
+++ src/scraping/trulia/sel_scrape/sel_latlong.py
@@ -1,8 +1,30 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+import pandas as pd 
+import os
+import pickle
 
 url = 'https://www.gps-coordinates.net/'
 
-browser = webdriver.Chrome()
-browser.get(url)
+driver = webdriver.Chrome()
+driver.get(url)
+
+def data_load():
+    pass
+
+def data_pickle():
+    pass
+
+def paste_keys(self, xpath, text):
+    os.system("echo %s| clip" % text.strip())
+    el = self.driver.find_element_by_xpath(xpath)
+    el.send_keys(Keys.CONTROL, 'v')
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
