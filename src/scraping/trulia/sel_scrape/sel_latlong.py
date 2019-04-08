e03b9f5d7aa3cbbe920f917566025337e050ab89
Austin Madert <austinmadert@Austins-MacBook-Pro.local>
2019-04-08 10:28:15 -0500

    Added comments and return statements to sel_scraper

--- src/scraping/trulia/sel_scrape/sel_latlong.py
+++ src/scraping/trulia/sel_scrape/sel_latlong.py
@@ -12,24 +12,54 @@ driver = webdriver.Chrome()
 driver.get(url)
 
 def data_load(path):
+    '''Reads a pickle object and loads pandas dataframe object from a given path
+    and then converts a slice of the dataframe for addresses into a list object
+
+    returns: list of addresses
+    '''
+
     df = pd.read_pickle(path)
     return list(df['adj_address'])
 
-def data_pickle():
-    pass
+
+def data_pickle(latlonglist):
+    '''Takes a list object of addresses, latitudes, and longitudes and writes
+    them to a pickle file for later use
+    
+    returns: None
+    '''
+
+    with open('latlonglist.pkl', 'wb') as f:
+        pickle.dump(latlonglist, f)
+    return None
+
 
 def paste_keys(self, xpath, text):
+    '''Takes an xpath for the selenium scraper and the text string and pastes
+    the text into the web page element
+
+    returns: None
+    '''
+
     os.system("echo %s| clip" % text.strip())
     el = self.driver.find_element_by_xpath(xpath)
     el.send_keys(Keys.CONTROL, 'v')
+    return None
+
 
 def collect_latlongs(addresslist, xpath):
+
     for address in addresslist:
+    
+    return latlonglist
 
 
 def main(path='../trulscraped_data.pkl', elements=xpath):
+    
+
     addresslist = data_load(path)
-    collect_latlongs(addresslist, xpath)
+    latlonglist = collect_latlongs(addresslist, xpath)
+    data_pickle(latlonglist)
 
 
 
