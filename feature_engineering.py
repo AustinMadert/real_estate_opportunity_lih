2d6adb1a2e98c93fc66bb5694054d48c2a208b54
Austin Madert <austinmadert@Austins-MacBook-Pro.local>
2019-04-10 10:59:31 -0500

    finished data import and cleaning functions

--- feature_engineering.py
+++ feature_engineering.py
@@ -18,62 +18,53 @@ def data_imports():
     pricedf = pd.DataFrame(prices, columns=['address', 'bathrooms', 'bedrooms', \
         'city_state_zip', 'house_type', 'price', 'sqft', 'url', 'price_per_sqft', \
         'adj_address', 'apts'])
-    newdf = lldf.merge(pricedf, left_on='address', right_on='adj_address', how='left')
+    train_df = lldf.merge(pricedf, left_on='address', right_on='adj_address', how='left')
 
-    return [lldf, pricedf, newdf]
+    return train_df, geodf
 
-def clean_dataframes():
-    
+def clean_dataframes(train_df, geodf):
 
-newdf.drop(newdf[newdf['bedrooms'] == 'Lot/Land'].index, inplace=True)
-newdf.drop(newdf[newdf['bathrooms'] == 'Condo'].index, inplace=True)
-newdf.drop(newdf[newdf['sqft'] == 0].index , inplace=True)
+    train_df.drop(train_df[train_df['bedrooms'] == 'Lot/Land'].index, inplace=True)
+    train_df.drop(train_df[train_df['bathrooms'] == 'Condo'].index, inplace=True)
+    train_df.drop(train_df[train_df['sqft'] == 0].index , inplace=True)
 
-newdf['bathrooms'] = newdf['bathrooms'].map(lambda x: str(x).replace('Bathrooms', '').replace('Bathroom', '').strip()).astype(float)
-newdf['bedrooms'] = newdf['bedrooms'].map(lambda x: str(x).replace('Bedrooms', '').replace('Bedroom', '').strip()).astype(float)
+    train_df['bathrooms'] = train_df['bathrooms'].map(lambda x: str(x)\
+        .replace('Bathrooms', '').replace('Bathroom', '').strip()).astype(float)
+    train_df['bedrooms'] = train_df['bedrooms'].map(lambda x: str(x)\
+        .replace('Bedrooms', '').replace('Bedroom', '').strip()).astype(float)
 
-X = newdf[['latitude', 'longitude']]
-y = newdf['price_per_sqft']
-X_train, X_test, y_train, y_test = train_test_split(X, y)
+    train_df.longitude = train_df.longitude.astype(float)
+    train_df.latitude = train_df.latitude.astype(float)
 
-newdf.longitude = newdf.longitude.astype(float)
-newdf.latitude = newdf.latitude.astype(float)
+    geodf.c_zip_code = geodf.c_zip_code.astype(str)
+    geodf.c_zip_code = geodf.c_zip_code.map(lambda x: x.replace('.0', ''))
 
-X1 = newdf[['latitude', 'longitude']]
-y1 = newdf['price_per_sqft']
-X1_train, X1_test, y1_train, y1_test = train_test_split(X1, y1)
-boost = GradientBoostingRegressor(learning_rate=0.01, n_estimators=800)
-boost.fit(X1_train, y1_train)
-boost.score(X1_test, y1_test)
+    geodf['spaces'] = ' '
+    geodf['adj_address'] = geodf.c_street_number + geodf.spaces +\
+        geodf.c_street + geodf.c_city + geodf.spaces + geodf.c_state +\
+        geodf.spaces + geodf.c_zip_code
 
-geodf.c_zip_code = geodf.c_zip_code.astype(str)
-geodf.c_zip_code = geodf.c_zip_code.map(lambda x: x.replace('.0', ''))
+    cleaningdf = geodf.drop(['address', 'address1', 'address2',\
+         'g_street_number', 'g_street', 'g_city', 'g_state','spaces',\
+         'g_unit', 'g_zip_code'], axis=1).copy()
 
-geodf['spaces'] = ' '
-geodf['adj_address'] = geodf.c_street_number + geodf.spaces + geodf.c_street + geodf.c_city + geodf.spaces + geodf.c_state + geodf.spaces + geodf.c_zip_code
+    bigdf = cleaningdf[pd.notnull(cleaningdf.lat)]
 
-cleaningdf = geodf.drop(['address', 'address1', 'address2', 'g_street_number', 'g_street', 'g_city', 'g_state','spaces', 'g_unit', 'g_zip_code'], axis=1).copy()
+    bigdf.drop_duplicates(subset='adj_address', inplace=True)
 
-bigdf = cleaningdf[pd.notnull(cleaningdf.lat)]
+    bigdf[bigdf['adj_address'].isnull()]
 
-bigdf.drop_duplicates(subset='adj_address', inplace=True)
+    bigdf.reset_index(inplace=True)
+    bigdf.drop(columns='index', inplace=True)
 
-bigdf.c_unit = bigdf.c_unit.fillna(-1, inplace=True).copy()
-newdf.apts = newdf.apts.fillna(-1, inplace=True).copy()
+    bigdf.iloc[207,4] = 'AUSTIN'
+    bigdf.iloc[207,10] = bigdf.iloc[207,1] + ' ' + bigdf.iloc[207, 2] +\
+         ' ' + bigdf.iloc[207,4] + ' ' + bigdf.iloc[207,5] +\
+         ' ' + bigdf.iloc[207,6]
 
-bigdf[bigdf['adj_address'].isnull()]
+    austin_df = bigdf[bigdf['c_city'] == 'AUSTIN'].copy()
 
-bigdf.reset_index(inplace=True)
-
-bigdf.iloc[207,4] = 'AUSTIN'
-bigdf.iloc[207,10] = bigdf.iloc[207,1] + ' ' + bigdf.iloc[207, 2] + ' ' + bigdf.iloc[207,4] + ' ' + bigdf.iloc[207,5] + ' ' + bigdf.iloc[207,6]
-bigdf.iloc[207,:]
-
-bigdf.drop(columns='index', inplace=True)
-
-austindf = bigdf[bigdf['c_city'] == 'AUSTIN'].copy()
-
-testdf = newdf[['latitude', 'longitude', 'price_per_sqft','price']].copy()
+    return train_df, austin_df
 
 def train_compute(train_df, train_X, train_y, compute_df, compute_X, compute_y):
     tx = train_df[train_X]
@@ -104,7 +95,7 @@ def pickle_dataframe(df, name):
 
 def main():
     data_imports()
-    
+
 
 
 
