8e89ac535f6446a3d0a717e4f2ba9bda6ab56f7a
Austin Madert <austinmadert@Austins-MacBook-Pro.local>
2019-04-10 14:35:21 -0500

    Finished creating geoplotting.py function

--- geoplotting.py
+++ geoplotting.py
@@ -2,23 +2,16 @@
 # coding: utf-8
 
 from mpl_toolkits.basemap import Basemap
-from matplotlib.collections import LineCollection
-import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import os
-from sklearn.cluster import KMeans
-from matplotlib.patches import Polygon
-from matplotlib.collections import PatchCollection
-import matplotlib.cm as cm
-import matplotlib.colors as colors
-import geopandas as gpd
 from sklearn.model_selection import train_test_split
 
 
 def create_geoplot(df, lowerlon=-98.10, upperlon=-97.47, lowerlat = 30.11, upperlat = 30.55,
-        shapefile_dir = "data/cb_2017_us_zcta510_500k", sample_data=True, sample_size=0.1):
+        shapefile_dir = "/Users/austinmadert/galvanize_repositories/real_estate_opportunity_lih/data/cb_2017_us_zcta510_500k",
+        sample_data=True, sample_size=0.1, colors=['b']):
 
     if sample_data:
         X_train, X_test = train_test_split(df, test_size=sample_size, stratify=df['labels'])
@@ -26,7 +19,9 @@ def create_geoplot(df, lowerlon=-98.10, upperlon=-97.47, lowerlat = 30.11, upper
     # Read in data.
     colormap = plt.cm.Purples 
 
-    os.chdir(shapefile_dir)
+    cwd = os.getcwd()
+    if cwd != shapefile_dir:
+        os.chdir(shapefile_dir)
 
     fig = plt.figure(figsize=(12,12))
     m = Basemap(
@@ -47,15 +42,6 @@ def create_geoplot(df, lowerlon=-98.10, upperlon=-97.47, lowerlat = 30.11, upper
     lats = X_test['lat'].to_numpy()
     lons = X_test['lon'].to_numpy()
     labels = X_test['labels'].to_numpy()
-
-    colors = []
-    for i in labels:
-        if i == 0:
-            colors.append('b')
-        elif i == 1:
-            colors.append('r')
-        else:
-            colors.append('y')
 
     x, y = m(lons, lats)
     plt.scatter(x, y, 12, marker='o', color=colors)
