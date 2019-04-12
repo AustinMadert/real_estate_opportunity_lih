0dc6f6198b327e88a82878268f21f99b41998241
Austin Madert <austinmadert@Austins-MacBook-Pro.local>
2019-04-11 20:20:32 -0500

    Debugging the boost_dataframe func and added loading code

--- feature_engineering.py
+++ feature_engineering.py
@@ -6,6 +6,9 @@ import geopandas as gpd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingRegressor
 from math import sin, cos, sqrt, atan2, radians
+from pickle import loads
+import warnings
+warnings.filterwarnings('ignore')
 
 
 def data_imports():
@@ -36,48 +39,47 @@ def clean_dataframes(train_df, geodf):
     train_df.longitude = train_df.longitude.astype(float)
     train_df.latitude = train_df.latitude.astype(float)
 
+    geodf.drop(['address', 'address1', 'address2',\
+         'g_street_number', 'g_street', 'g_city', 'g_state',\
+         'g_unit', 'g_zip_code'], axis=1, inplace=True)
+
     geodf.c_zip_code = geodf.c_zip_code.astype(str)
     geodf.c_zip_code = geodf.c_zip_code.map(lambda x: x.replace('.0', ''))
 
-    geodf['spaces'] = ' '
-    geodf['adj_address'] = geodf.c_street_number + geodf.spaces +\
-        geodf.c_street + geodf.c_city + geodf.spaces + geodf.c_state +\
-        geodf.spaces + geodf.c_zip_code
+    bigdf = geodf[pd.notnull(geodf.lat)]
 
-    cleaningdf = geodf.drop(['address', 'address1', 'address2',\
-         'g_street_number', 'g_street', 'g_city', 'g_state','spaces',\
-         'g_unit', 'g_zip_code'], axis=1).copy()
+    bigdf.iloc[207,4] = 'AUSTIN'
 
-    bigdf = cleaningdf[pd.notnull(cleaningdf.lat)]
+    bigdf['adj_address'] = geodf.c_street_number + ' ' + geodf.c_street +\
+         geodf.c_city + ' ' + geodf.c_state + ' ' + geodf.c_zip_code
 
     bigdf.drop_duplicates(subset='adj_address', inplace=True)
-
-    bigdf[bigdf['adj_address'].isnull()]
 
     bigdf.reset_index(inplace=True)
     bigdf.drop(columns='index', inplace=True)
 
-    bigdf.iloc[207,4] = 'AUSTIN'
-    bigdf.iloc[207,10] = bigdf.iloc[207,1] + ' ' + bigdf.iloc[207, 2] +\
-         ' ' + bigdf.iloc[207,4] + ' ' + bigdf.iloc[207,5] +\
-         ' ' + bigdf.iloc[207,6]
-
     city_df = bigdf[bigdf['c_city'] == 'AUSTIN'].copy()
 
     # get list of coordinates
+    print('Getting list of city address coordinates...')
     subset = city_df[['lat', 'lon']]
     tuples = [tuple(x) for x in subset.values]
     distances = [haversine_to_downtown(coord) for coord in tuples]
     city_df['dist_to_downtown'] = distances
-    austin_df = city_df[city_df['dist_to_downtown'] < (city_df.dist_to_downtown.std() * 4)].copy()
 
+    print('Getting list of city bus stop coordinates...')
     stops = gpd.read_file('data/Shapefiles_20-_20JANUARY_202018/Stops/Stops.shp')
-    subs = stops[['LATITUDE', 'LONGITUDE']]
+    subs = stops[['LATITUDE', 'LONGITUDE']].dropna()
     tups = [tuple(x) for x in subs.values]
     min_transport_dist = [min([two_point_haversine(stop, address) for stop in tups]) for address in tuples]
-    austin_df['min_dist_to_transport'] = min_transport_dist
+    city_df['min_dist_to_transport'] = min_transport_dist
 
-    return train_df, austin_df
+    intown_df = city_df[city_df['dist_to_downtown'] < (city_df.dist_to_downtown.std() * 4)].copy()
+
+    pickle_dataframe(train_df, 'train_df.pkl')
+    pickle_dataframe(intown_df, 'intown_df.pkl')
+
+    return None
 
 
 def haversine_to_downtown(point):
@@ -117,26 +119,35 @@ def train_compute(train_df, train_features, train_labels, compute_df, compute_X,
     return score
 
 
-def boost_dataframe(train_df, austin_df):        
+def boost_dataframe():        
+    
+    fl1 = open('train_df.pkl')
+    train_df = loads(fl1)
+    fl1.close()
+
+    fl2 = open('intown_df.pkl')
+    intown_df = loads(fl2)
+    fl2.close()
+    
     #compute price
     price_score = train_compute(
         train_df,
         ['latitude', 'longitude', 'price_per_sqft'], 
         'price',
-        austin_df, 
+        intown_df, 
         ['lat', 'lon', 'price_per_sqft'], 
         'price'
         )
 
     #compute sqft
-    austin_df['sqft'] = austin_df['price'] / austin_df['price_per_sqft']
+    intown_df['sqft'] = intown_df['price'] / intown_df['price_per_sqft']
 
     #compute bathrooms
     bathroom_score = train_compute(
         train_df, 
         ['latitude', 'longitude', 'price_per_sqft', 'price', 'sqft'],
         'bathrooms', 
-        austin_df, 
+        intown_df, 
         ['lat', 'lon', 'price_per_sqft', 'price', 'sqft'], 
         'bathrooms'
         )
@@ -146,19 +157,19 @@ def boost_dataframe(train_df, austin_df):
         train_df, 
         ['latitude', 'longitude', 'price_per_sqft', 'price', 'sqft', 'bathrooms'],
         'bedrooms', 
-        austin_df, 
+        intown_df, 
         ['lat', 'lon', 'price_per_sqft', 'price', 'sqft', 'bathrooms'], 
         'bedrooms'
         )
 
     #force bedrooms into int in order to be conservative in estimate
-    austin_df.bedrooms = austin_df.bedrooms.astype(int)
-    austin_df['price_per_bed'] = austin_df['price'] / austin_df['bedrooms']
+    intown_df.bedrooms = intown_df.bedrooms.astype(int)
+    intown_df['price_per_bed'] = intown_df['price'] / intown_df['bedrooms']
 
     print('Price score: {}\nBathroom score: {}\nBedroom score: {}'\
         .format(price_score, bathroom_score, bedroom_score))
 
-    return austin_df
+    return intown_df
 
 
 def pickle_dataframe(df, name):
@@ -169,10 +180,16 @@ def pickle_dataframe(df, name):
 
 
 def main():
-    train_df, geo_df = data_imports()
-    clean_train, clean_aus = clean_dataframes(train_df, geo_df)
-    boosted_aus = boost_dataframe(clean_train, clean_aus)
-    pickle_dataframe(boosted_aus, 'boosted_dataset.pkl')
+    response = input('Would like to skip cleaning and use pickle? ')
+    if response == 'y':
+        train_df, geo_df = data_imports()
+        clean_dataframes(train_df, geo_df)
+        boosted_aus = boost_dataframe()
+        pickle_dataframe(boosted_aus, 'boosted_dataset.pkl')
+    else:
+        boosted_aus = boost_dataframe()
+        pickle_dataframe(boosted_aus, 'boosted_dataset.pkl')
+    
     return None
 
 
